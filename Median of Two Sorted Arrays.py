900ec4b91e59f7fc12828247ae3f7c25e677bceb
Dmitry <onishchuk.dmitry@gmail.com>
2021-10-18 12:51:47 +0300

    reformat code

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -5,41 +5,37 @@ The overall run time complexity should be O(log (m+n))
 """
 
 
-class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
-        merge = []
-        i = j = 0
-        while True:
-            if len(nums1) == i:
-                merge += nums2[j:]
-                break
-            elif len(nums2) == j:
-                merge += nums1[i:]
-                break
-            if nums1[i] >= nums2[j]:
-                merge += [nums2[j]]
-                j += 1
-            else:
-                merge += [nums1[i]]
-                i += 1
-        if not len(merge) % 2:
-            return (merge[(len(merge) // 2) - 1] + merge[(len(merge) // 2)]) / 2
+def find_median_sorted_arrays(nums1, nums2):
+    merge = []
+    i = j = 0
+    while True:
+        if len(nums1) == i:
+            merge += nums2[j:]
+            break
+        elif len(nums2) == j:
+            merge += nums1[i:]
+            break
+        if nums1[i] >= nums2[j]:
+            merge += [nums2[j]]
+            j += 1
         else:
-            return merge[(len(merge) // 2)]
+            merge += [nums1[i]]
+            i += 1
+    if not len(merge) % 2:
+        return (merge[(len(merge) // 2) - 1] + merge[(len(merge) // 2)]) / 2
+    else:
+        return merge[(len(merge) // 2)]
 
 
-res = Solution()
-print(res.findMedianSortedArrays(nums1=[2], nums2=[2]))
+def find_median_sorted_arrays_2(nums1, nums2):
+    merge = sorted(nums1 + nums2)
+    if not len(merge) % 2:
+        return (merge[(len(merge) // 2) - 1] + merge[(len(merge) // 2)]) / 2
+    else:
+        return merge[(len(merge) // 2)]
 
 
-class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
-        merge = sorted(nums1 + nums2)
-        if not len(merge) % 2:
-            return (merge[(len(merge) // 2) - 1] + merge[(len(merge) // 2)]) / 2
-        else:
-            return merge[(len(merge) // 2)]
+first_list, second_list = [-10, 0, 3, 5, 16, 88, 100], [0, 2, 7, 42, 77, 99]
 
-
-res = Solution()
-print(res.findMedianSortedArrays(nums1=[2], nums2=[2]))
+print(find_median_sorted_arrays_2(nums1=first_list, nums2=second_list))
+print(find_median_sorted_arrays(nums1=first_list, nums2=second_list))
